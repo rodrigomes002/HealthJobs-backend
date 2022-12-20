82def0d99d2475702b6f8610aa23d63d31698dce
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2022-12-19 22:29:22 -0300

    Add project files.

---
+++ HealthJobs.Domain/Vagas/Vaga.cs
@@ -0,0 +1,22 @@
+﻿namespace HealthJobs.Domain.Vagas
+{
+    public class Vaga
+    {
+        public string Empresa { get; private set; }
+        public string Cargo { get; private set; }
+        public int Salario { get; private set; }
+        public string Descricao { get; private set; }
+
+        public Vaga(string empresa, string cargo, int salario, string descricao)
+        {
+            if (String.IsNullOrEmpty(empresa)) throw new ArgumentException("Empresa inválida!");
+            if (String.IsNullOrEmpty(cargo)) throw new ArgumentException("Cargo inválido!");
+            if (String.IsNullOrEmpty(descricao)) throw new ArgumentException("Descrição inválida!");
+            
+            this.Empresa = empresa;
+            this.Cargo = cargo;
+            this.Salario = salario;
+            this.Descricao = descricao;
+        }
+    }
+}
---
+++ HealthJobs.Test/Vagas/VagaTest.cs
@@ -0,0 +1,78 @@
+using ExpectedObjects;
+using HealthJobs.Domain.Vagas;
+
+namespace HealthJobs.Test.Vagas
+{
+    public class VagaTest
+    {
+        [Fact]
+        public void DeveCriarVaga()
+        {
+            var vagaEsperada = new
+            {
+                Empresa = "Clinica de saúde",
+                Cargo = "Fisioterapeuta",
+                Salario = 10000,
+                Descricao = "Uma descrição"
+            };
+
+            var vaga = new Vaga(vagaEsperada.Empresa, vagaEsperada.Cargo, vagaEsperada.Salario, vagaEsperada.Descricao);
+
+            vagaEsperada.ToExpectedObject().ShouldMatch(vaga);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCriarVagaComEmpresaInvalida(string empresa)
+        {
+            var vagaEsperada = new
+            {
+                Empresa = empresa,
+                Cargo = "Fisioterapeuta",
+                Salario = 10000,
+                Descricao = "Uma descrição"
+            };
+
+            var ex = Assert.Throws<ArgumentException>(() => new Vaga(vagaEsperada.Empresa, vagaEsperada.Cargo, vagaEsperada.Salario, vagaEsperada.Descricao));
+
+            Assert.Equal("Empresa inválida!", ex.Message);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCriarVagaComCargoInvalido(string cargo)
+        {
+            var vagaEsperada = new
+            {
+                Empresa = "Clinica de saúde",
+                Cargo = cargo,
+                Salario = 10000,
+                Descricao = "Uma descrição"
+            };
+
+            var ex = Assert.Throws<ArgumentException>(() => new Vaga(vagaEsperada.Empresa, vagaEsperada.Cargo, vagaEsperada.Salario, vagaEsperada.Descricao));
+
+            Assert.Equal("Cargo inválido!", ex.Message);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCriarVagaComDescricaoInvalida(string descricao)
+        {
+            var vagaEsperada = new
+            {
+                Empresa = "Clinica de saúde",
+                Cargo = "Fisioterapeuta",
+                Salario = 10000,
+                Descricao = descricao
+            };
+
+            var ex = Assert.Throws<ArgumentException>(() => new Vaga(vagaEsperada.Empresa, vagaEsperada.Cargo, vagaEsperada.Salario, vagaEsperada.Descricao));
+
+            Assert.Equal("Descrição inválida!", ex.Message);
+        }
+    }
+}
